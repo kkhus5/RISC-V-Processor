90c160c24e98fdd172aa529826b0de053047e968
Class Account <eecs151-acb@c125m-16.EECS.Berkeley.EDU>
2020-11-02 19:26:09 -0800

    finished ALU?

--- src/ALU.v
+++ src/ALU.v
@@ -18,5 +18,21 @@ module ALU(
 );
 
     // Implement your ALU here, then delete this comment
-
+    always @(*) begin
+	case (ALUop)
+		`ALU_ADD: Out = A + B;
+		`ALU_SUB: Out = A - B;
+		`ALU_AND: Out = A & B;
+		`ALU_OR : Out = A | B;
+		`ALU_XOR: Out = A ^ B;
+		`ALU_SLT: Out = ($signed(A) < $signed(B))? 32'd1: 32'd0;
+		`ALU_SLTU:Out = (A < B)? 32'd1: 32'd0;
+		`ALU_SLL: Out = A << B[4:0];
+		`ALU_SRA: Out = A >>> B[4:0];
+		`ALU_SRL: Out = A >> B[4:0];
+		`ALU_COPY_B: Out = B;
+		`ALU_COPY_A: Out = A;
+		`ALU_XXX: Out = 32'd0;
+		default : Out = 32'dx;
+	endcase
 endmodule
