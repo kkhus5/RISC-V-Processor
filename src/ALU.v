36cd329b1f72beb3d289419744e30bbea1456e5c
Class Account <eecs151-acb@c125m-16.EECS.Berkeley.EDU>
2020-11-03 17:12:21 -0800

    fixed SRA bug

--- src/ALU.v
+++ src/ALU.v
@@ -28,11 +28,12 @@ module ALU(
 		`ALU_SLT: Out = ($signed(A) < $signed(B))? 32'd1: 32'd0;
 		`ALU_SLTU:Out = (A < B)? 32'd1: 32'd0;
 		`ALU_SLL: Out = A << B[4:0];
-		`ALU_SRA: Out = A >>> B[4:0];
+		`ALU_SRA: Out = $signed(A) >>> B[4:0];
 		`ALU_SRL: Out = A >> B[4:0];
 		`ALU_COPY_B: Out = B;
 		`ALU_COPY_A: Out = A;
 		`ALU_XXX: Out = 32'd0;
 		default : Out = 32'dx;
 	endcase
+    end
 endmodule
