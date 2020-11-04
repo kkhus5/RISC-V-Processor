6d9b60676675d51e936ea570c442481dc95555d9
Class Account <eecs151-acb@c125m-16.EECS.Berkeley.EDU>
2020-11-03 18:39:54 -0800

    finished vector testbench

--- src/ALUTestVectorTestbench.v
+++ src/ALUTestVectorTestbench.v
@@ -65,7 +65,7 @@ module ALUTestVectorTestbench();
     // testvector input file, which you can find with the command:
     // % wc -l ../sim/tests/testvectors.input
     // //////////////////////////////////////////////////////////////
-    localparam testcases = 26;
+    localparam testcases = 30;
 
     reg [106:0] testvector [0:testcases-1]; // Each testcase has 108 bits:
     // 64 for A and B, 32 for REFout, 6 for
